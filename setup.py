0064340bcaa7aa2ed2b409ec9a56016da32d090a
WKS-ADM-Bin Ilyas <WKS-ADM-binilyas@lan.meyerwerft.de>
2022-03-29 21:37:33 +0200

    Init the repo with cookiecutter datascience structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Experimental project for ML',
+    author='Eushay',
+    license='MIT',
+)
